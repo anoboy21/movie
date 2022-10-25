306a881e36457e0cde4a65d0a90e9c5c4538b54b
anoboy21 <109586478+anoboy21@users.noreply.github.com>
2022-10-25 17:14:49 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
+    TMDB_API_KEY: process.env.08b5dc7d8539d5f952232df490e5de4e,
   },
   images: {
     domains: ['image.tmdb.org'],
